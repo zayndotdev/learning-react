9fa36652998cbb945cde777d4680df5691aeda11
Zayn Raza <zaynraza.dev@gmail.com>
2025-06-28 03:42:24 +0500

    resetup everything

---
+++ basic-context/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center bg-neutral-900 h-screen">
+      <h1 className="text-7xl text-neutral-100">
+        console.log("It's working...");
+      </h1>
+    </div>
+  );
+}
+
+export default App;
